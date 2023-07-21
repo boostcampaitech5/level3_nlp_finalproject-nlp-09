833273aac995ed9403f2976d5f7f5b0e7b25ccdc
traintogpb <kaiba0514@gmail.com>
2023-07-21 23:26:43 +0900

    feat: make markdown-style pdf from stt result
    -md2pdf와 text_to_pdf를 통해 생성된 전사/요약/퀴즈 중 선택하여 PDF 생성
    
    #23

--- backend/main.py
+++ backend/main.py
@@ -3,13 +3,14 @@ import tempfile
 import shutil
 import os
 import sys
-from typing import Union
+from typing import Union, List
 import asyncio
+import datetime
 
 from pydantic import BaseModel
 from fastapi import FastAPI, Depends, HTTPException, Form, File, UploadFile, Request, Response, status, BackgroundTasks
 from fastapi.security import OAuth2PasswordRequestForm
-from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
@@ -29,6 +30,11 @@ sys.path.append('./ml_functions')  # nopep8
 from ml_functions.stt import transcribe, transcribe_async, transcribe_test
 from ml_functions.summary import summarize, summarize_async, summarize_test
 from ml_functions.qna import questionize, questionize_async, questionize_test
+
+# pdf export
+from weasyprint import HTML, CSS
+from weasyprint.fonts import FontConfiguration
+from markdown2 import markdown_path
 
 # login
 from login import get_current_user, create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
@@ -293,6 +299,60 @@ async def change_qna(request: Body, db: Session = Depends(get_db)):
     qna = get_qna_by_id(db, request.qna_id)
     change_history_qna(db, qna, request.question, request.answer)
     return {"type": True, "message": "change success"}
+
+
+# TODO: Markdown to PDF 변환기
+def md2pdf(filename, output):
+    html = markdown_path(filename, encoding="utf-8")
+    print(html)
+    html_utf8 = f'<meta charset="utf-8">\n{html}'
+    css_with_font_family = CSS(string='@font-face { font-family: NanumSquareR; src: /usr/share/fonts/truetype/myfonts/NanumSquareR.ttf; }')
+    HTML(string=html_utf8, encoding="utf-8").write_pdf(output, stylesheets=[css_with_font_family])
+
+
+# TODO: Markdown 형식 텍스트 만들기
+def text_to_pdf(output_filename: str, content_types: List, history: History, db: Session = Depends(get_db)):
+    md_title, md_contents = f"# {history.title}", ""
+    if 'transcription' in content_types:
+        md_history = f"## 속기\n\n{history.transcription}"
+        md_contents += md_history
+    if 'summary' in content_types:
+        md_summary = f"## 요약\n\n{history.summary}"
+        md_contents += md_summary
+    if 'qnas' in content_types:
+        qnas = get_qnas_by_history_id(db, history.history_id)
+        md_qnas = "\n\n## 퀴즈"
+        for qna_number, qna in enumerate(qnas):
+            md_qna = f"\n### 퀴즈 {qna_number+1}.\n{qna.question}\n\n{qna.answer}"
+            md_qnas += md_qna
+        md_contents += md_qnas
+
+    md_text = md_title + "\n\n" + md_contents
+
+    temp_md_file = tempfile.NamedTemporaryFile(mode="w", delete=True, suffix=".md")
+    with open(temp_md_file.name, "w", encoding="utf-8") as temp_file:
+        temp_file.write(md_text)
+    md2pdf(temp_md_file.name, output_filename)
+
+
+# TODO: 속기본 PDF 내보내기 엔드포인트
+@app.post("/history/export_pdf")
+async def export_pdf(request: Body, db: Session = Depends(get_db)):
+    info = get_current_user(request.access_token)
+    if info["message"] != "Valid":
+        return {"type": False, "message": info["message"]}
+    
+    current_time = datetime.datetime.now().strftime("%y%m%d%H%M%S")
+    pdf_filename = f"lecnrec_{current_time}.pdf"
+    content_types = ['transcription', 'qnas']
+    history = get_history_by_id(db, request.history_id)
+    text_to_pdf(
+        output_filename=pdf_filename,
+        content_types=content_types,
+        history=history,
+        db=db
+    )
+    return FileResponse(pdf_filename, filename=pdf_filename, headers={"Content-Disposition": "attachment"})
 
 
 if __name__ == "__main__":
