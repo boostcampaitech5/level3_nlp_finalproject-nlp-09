6dacaa0ae4c1876d5059214d337930238a119540
WorldBestProgrammer <dons97@naver.com>
2023-07-22 04:49:38 +0000

    feat: add edit button
    #10

--- frontend/src/components/History.js
+++ frontend/src/components/History.js
@@ -63,26 +63,28 @@ const History = ( { isActive, id, history, onClickHistory } ) => {
           </button>
 
           { isActive ? (
-            <button onClick={ onClickDel } className="p-1 hover:text-white">
-              <svg
-                stroke="currentColor"
-                fill="none"
-                strokeWidth={ 2 }
-                viewBox="0 0 24 24"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="h-4 w-4"
-                height="1em"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <polyline points="3 6 5 6 21 6" />
-                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-                <line x1={ 10 } y1={ 11 } x2={ 10 } y2={ 17 } />
-                <line x1={ 14 } y1={ 11 } x2={ 14 } y2={ 17 } />
-              </svg>
-            </button>
-          )
+            <div class="flex right-1 z-10 text-gray-300 visible">
+              <button class="p-1 hover:text-white"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg></button>
+              <button onClick={ onClickDel } className="p-1 hover:text-white">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  strokeWidth={ 2 }
+                  viewBox="0 0 24 24"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="h-4 w-4"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <polyline points="3 6 5 6 21 6" />
+                  <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                  <line x1={ 10 } y1={ 11 } x2={ 10 } y2={ 17 } />
+                  <line x1={ 14 } y1={ 11 } x2={ 14 } y2={ 17 } />
+                </svg>
+              </button>
+            </div> )
             : null
           }
 
